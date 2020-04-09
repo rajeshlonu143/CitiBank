3c645efb7376c79768d3e6bf110760b8278c2532
DEV2 <rajeshlonu143@gmail.com>
2020-04-09 14:32:45 +0530

    2nd commitv by dev1

--- CBProj1/src/com/nt/bb/CityBankProj1.java
+++ CBProj1/src/com/nt/bb/CityBankProj1.java
@@ -4,6 +4,7 @@ public class CityBankProj1 {
 	public static void main(String[] args) {
 		System.out.println("Rajesh Das no 1");
 		System.out.println("I am devloper1");
+		System.out.println("CityBankProj1.main()");
 	}
 
 }
